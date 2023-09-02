074fac7776ad19b4b8f437f5ecba99423acd877d
Denis <den.zhernov@yandex.ru>
2023-09-02 07:07:26 +0300

    add file + mod js

--- js/section2/scriptLes2.js
+++ js/section2/scriptLes2.js
@@ -3,7 +3,7 @@
 let numberA = 5;
 let numberB = 2.5;
 
-//console.log(numberA/0); //infinity
+//console.log(numberA/0); //infinity - бесконечность
 //console.log('string' * 9); //NaN - не число
 
 const persone = "Alex"; //string
@@ -18,11 +18,22 @@ let und;
 const obj = {
     name: "Denis",
     age: 24,
-    isMariend: false
+    isMariend: false,
+    1: 2
 };
 
 console.log(obj.name);
 console.log(obj["name"]);
+console.log(obj["1"]);
+console.log(obj[1]);
+
+//добавление свойства  в объект
+obj.b = "Add";
+obj['d'] = "Add2"; 
+
+console.log(obj.b);
+console.log(obj['d']);
+
 
 //arrray
 let arr = ['1','2', 3,'4','5','6', { a: 34}, 78, [1,2]];
